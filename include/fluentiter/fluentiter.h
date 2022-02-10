bfa8542acdc9bc5f590e9b15956a37c9c660754c
Maarten Flippo <maartenflippo@outlook.com>
2022-02-10 16:00:09 +0100

    Implemented FluentIter, which converts native iterators to our own Iterator<T>.

--- include/fluentiter/fluentiter.h
+++ include/fluentiter/fluentiter.h
@@ -1,1 +1,41 @@
 #pragma once
+
+#include <iterator>
+#include <optional>
+
+namespace fluentiter {
+
+    template <typename T>
+    class Iterator {
+    public:
+        virtual ~Iterator() = default;
+
+        /**
+         * Get the current item and advance the iterator. If has_next() is true, then this will return the current item,
+         * otherwise an empty optional.
+         *
+         * @return The current item, or an empty optional if the iterator ran out of items.
+         */
+        virtual std::optional<T> next() = 0;
+    };
+
+    template <class InputIt, typename T = typename std::iterator_traits<InputIt>::value_type>
+    class FluentIter final : public Iterator<T> {
+    private:
+        InputIt m_current;
+        InputIt m_end;
+
+    public:
+        FluentIter(InputIt current, InputIt end) : m_current(current), m_end(end) {}
+
+        std::optional<T> next() override {
+            if (m_current == m_end)
+                return {};
+
+            T value = *m_current;
+            ++m_current;
+            return value;
+        }
+    };
+
+}
--- test/source/fluentiter.cpp
+++ test/source/fluentiter.cpp
@@ -1,2 +1,13 @@
 #include <doctest/doctest.h>
 #include <fluentiter/fluentiter.h>
+#include <vector>
+
+TEST_CASE("FluentIter yields the items in an input iterator") {
+    std::vector<int> items{1, 2, 3};
+    fluentiter::FluentIter iter(items.begin(), items.end());
+
+    CHECK_EQ(1, *iter.next());
+    CHECK_EQ(2, *iter.next());
+    CHECK_EQ(3, *iter.next());
+    CHECK_FALSE(iter.next().has_value());
+}
