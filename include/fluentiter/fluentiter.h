d40ff77f62b2c6b17424952c47b75503f5b14e77
Maarten Flippo <maartenflippo@outlook.com>
2022-02-12 15:20:05 +0100

    Remove unused header

--- include/fluentiter/fluentiter.h
+++ include/fluentiter/fluentiter.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <concepts>
 #include <iterator>
 #include <optional>
 #include <functional>
--- include/fluentiter/ops/map.h
+++ include/fluentiter/ops/map.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <concepts>
-
 namespace fluentiter {
     template<class Input, typename T>
     class Iterator;
